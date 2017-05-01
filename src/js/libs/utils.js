4766d0f13f9b91c67a8b953347aefef2da687f9d
Matheus Mesquita <matheusmesquita.168@gmail.com>
2017-05-01 13:00:55 -0300

    removed the test example from utils module

--- src/js/libs/utils.js
+++ src/js/libs/utils.js
@@ -1,10 +1,18 @@
 (function(global){
   "use strict";
 
-  global.utils = {
-    getLastIndex: function(arr){
-      return arr.length ? arr.length - 1 : 0;
+  if ( !global.modules ) global.modules = Object.create(null);
+
+  global.modules.utils = {
+    fetchJson: function(jsonFile){
+      return new Promise((resolve, reject) => {
+        fetch(jsonFile)
+          .then(response => response.json())
+          .then(resolve)
+          .catch(reject);
+      });
     }
   };
+
 
 })(window);
